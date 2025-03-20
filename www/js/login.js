ddc40325e6ac1122cf10240942e7e9c68c6ce8c4
Ian Souza Freire <iantmessenger@gmail.com>
2025-03-20 15:34:34 -0300

    algo1

--- www/js/login.js
+++ www/js/login.js
@@ -1,19 +1,5 @@
 document.addEventListener('deviceready', onDeviceReady, false);
-document.getElementById('Show').addEventListener('click', ShowPassword, false);
 
 function onDeviceReady() {
     
-}
-
-function ShowPassword() {
-    var btn = document.getElementById("Password");
-    var img = document.getElementById("Show-img");
-
-    if (btn.type == "password") {
-        btn.type = "text";
-        img.src = "img/hide.png";
-    } else {
-        btn.type = "password";
-        img.src = "img/view.png";
-    }
 }
